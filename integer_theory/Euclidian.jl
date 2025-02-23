0482ee3c20f307406754bf9095d2813450fcc84f
isaiahpettingill <isaiah@redpeppersoftware.com>
2025-02-22 17:29:32 -0700

    euclidian algorithm

---
+++ integer_theory/Euclidian.jl
@@ -0,0 +1,14 @@
+function GCD(a::Integer, b::Integer)::Integer
+    if b == 0
+        return a 
+    end
+    return GCD(b, a % b)
+end
+
+function Z_LinearCombo(a::Integer, b::Integer)::Tuple{Integer, Integer}
+    if b == 0
+        return (1, 0)
+    end
+    x, y = Z_LinearCombo(b, a % b)
+    return (y, x - div(a, b) * y)
+end
